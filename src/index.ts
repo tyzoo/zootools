a9b1c647279f5a23cecb2e4cf2942a50c5625083
Tyler Russell <tyler.russell.design@gmail.com>
2022-03-31 12:45:41 -0400

    deploy script

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,17 @@ export { AlertSystem } from './classes/AlertSystem'
 export { TransformGatherer } from './classes/TransformGatherer'
 
 /**
- * Utils
+ * Various Utils
  */
 export { 
     makeid, 
     removeLineBreaks,
+    randomInt,
+    first,
+    last,
+    sample,
+    pluck,
+    groupBy,
+    formatNumber,
+    formatCompactNumber
 } from './utils/index'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,11 +4,55 @@ export function removeLineBreaks(str: string): string {
 
 export function makeid(length: number): string {
 	var result = '';
-	var characters =
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	var charactersLength = characters.length;
 	for (var i = 0; i < length; i++) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
 	}
 	return result;
+}
+
+export function randomInt(min:number, max: number) {
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export function first(array: any[], n = 1){
+	if( n === 1 ) return array[0]
+	return array.filter( ( _: any, idx: number ) => idx < n )
+}
+
+export function last(array: any[], n = 1){
+	if( n === 1 ) return array[array.length - 1]
+	return array.filter( ( _: any, idx: number ) => (array.length-idx) <= n )
+}
+
+export function sample(array: any[]){
+	return array[randomInt(0, array.length-1)]
+}
+
+export function pluck(array: any[], key: string){
+	return array.map(element=>element[key])
+}
+
+export function groupBy(array: any[], key: string){
+	return array.reduce((group, element) => {
+		const keyValue = element[key]
+		return {
+			...group, 
+			[keyValue]: [...(group[keyValue] ?? []), element]
+		}
+	})
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat(undefined);
+
+export function formatNumber(number:number){
+	NUMBER_FORMATTER.format(number);
+}
+const COMPACT_NUMBER_FORMATTER = new Intl.NumberFormat(undefined, {
+	notation: "compact"
+} as Intl.NumberFormatOptions);
+
+export function formatCompactNumber(number:number){
+	COMPACT_NUMBER_FORMATTER.format(number);
 }
