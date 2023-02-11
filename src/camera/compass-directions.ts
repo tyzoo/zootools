e2c808be63cbfdde64f04ed0da13c854afc4c426
Tyler Russell <33195081+tyzoo@users.noreply.github.com>
2023-02-11 15:51:53 -0500

    camera utils-compass dirs

---
+++ src/camera/compass-directions.ts
@@ -0,0 +1,28 @@
+export type CompassDirections = "n" | "s" | "e" | "w" | "ne" | "nw" | "se" | "sw";
+
+export function getCompassDirection(cameraRotation: Quaternion): CompassDirections {
+    const cameraEuler = cameraRotation.eulerAngles;
+    const yaw = cameraEuler.y;
+    if (yaw >= 337.5 || yaw < 22.5) {
+        return "n";
+    } else if (yaw >= 22.5 && yaw < 67.5) {
+        return "ne";
+    } else if (yaw >= 67.5 && yaw < 112.5) {
+        return "e";
+    } else if (yaw >= 112.5 && yaw < 157.5) {
+        return "se";
+    } else if (yaw >= 157.5 && yaw < 202.5) {
+        return "s";
+    } else if (yaw >= 202.5 && yaw < 247.5) {
+        return "sw";
+    } else if (yaw >= 247.5 && yaw < 292.5) {
+        return "w";
+    } else if (yaw >= 292.5 && yaw < 337.5) {
+        return "nw";
+    }
+    return "n";
+}
+
+export function getCameraCompassDirection(): CompassDirections{
+    return getCompassDirection(Camera.instance.rotation);
+}
--- src/index.ts
+++ src/index.ts
@@ -6,56 +6,61 @@
 /**
  * Classes
  */
-import { AlertSystem } from './classes/AlertSystem'
-import { CallbackDebouncer } from './classes/CallbackDebouncer'
-import { OnMove } from './classes/OnMove'
-import { SignedFetchAPI } from './classes/SignedFetch'
-import { SoundPlayer } from './classes/SoundPlayer'
-import { TransformGatherer } from './classes/TransformGatherer'
-import { UserInfo } from './classes/UserInfo'
+export { AlertSystem } from './classes/AlertSystem'
+export { CallbackDebouncer } from './classes/CallbackDebouncer'
+export { OnMove } from './classes/OnMove'
+export { SignedFetchAPI } from './classes/SignedFetch'
+export { SoundPlayer } from './classes/SoundPlayer'
+export { TransformGatherer } from './classes/TransformGatherer'
+export { UserInfo } from './classes/UserInfo'
 
 /**
  * Entities
  */
-import { AnimatedModel } from './entities/AnimatedModel'
-import { Booth, IBoothProps } from './entities/Booth/Booth'
-import { Elements } from './entities/Elements'
-import { Image } from './entities/Image'
-import { Model, IModelProps } from './entities/Model'
-import { ModelGroup, IModelGroupProps } from './entities/ModelGroup'
-import { POAPBooth, IPOAPBoothProps } from './entities/Booth/POAPBooth'
-import { Sound } from './entities/Sound'
-import { WearableBooth, IWearableBoothProps } from './entities/Booth/WearableBooth'
-import { ZooTools_Metronome } from './entities/Metronome/Metronome'
-import { ZooTools_Metronome_ISubscription } from './entities/Metronome/types'
-import { ZooTools_ControlBoard } from './entities/ControlBoard/ControlBoard'
-import { ZooTools_ControlBoardButton } from './entities/ControlBoard/components/ControlBoardButton'
-import { ZooTools_ControlBoardMarker } from './entities/ControlBoard/components/ControlBoardMarker'
-import { ZooTools_MetronomeOutput as ZooTools_ControlBoardOutput } from './entities/Metronome/MetronomeOutput'
+export { AnimatedModel } from './entities/AnimatedModel'
+export { Booth, IBoothProps } from './entities/Booth/Booth'
+export { Elements } from './entities/Elements'
+export { Image } from './entities/Image'
+export { Model, IModelProps } from './entities/Model'
+export { ModelGroup, IModelGroupProps } from './entities/ModelGroup'
+export { POAPBooth, IPOAPBoothProps } from './entities/Booth/POAPBooth'
+export { Sound } from './entities/Sound'
+export { WearableBooth, IWearableBoothProps } from './entities/Booth/WearableBooth'
+export { ZooTools_Metronome } from './entities/Metronome/Metronome'
+export { ZooTools_Metronome_ISubscription } from './entities/Metronome/types'
+export { ZooTools_ControlBoard } from './entities/ControlBoard/ControlBoard'
+export { ZooTools_ControlBoardButton } from './entities/ControlBoard/components/ControlBoardButton'
+export { ZooTools_ControlBoardMarker } from './entities/ControlBoard/components/ControlBoardMarker'
+export { ZooTools_MetronomeOutput as ZooTools_ControlBoardOutput } from './entities/Metronome/MetronomeOutput'
 
 /**
  * Systems
  */ 
-import { ISystemCallback } from './systems/ISystemCallback'
-import { Interval } from './systems/Interval'
-import { PersistUntil } from './systems/PersistUntil'
-import { RotateSystem } from './systems/RotateSystem'
-import { ZooTools_AutoDanceZone, ZooTools_AutoEmoter, AutoEmoteSystem } from './systems/Emote'
+export { ISystemCallback } from './systems/ISystemCallback'
+export { Interval } from './systems/Interval'
+export { PersistUntil } from './systems/PersistUntil'
+export { RotateSystem } from './systems/RotateSystem'
+export { ZooTools_AutoDanceZone, ZooTools_AutoEmoter, AutoEmoteSystem } from './systems/Emote'
+
+/**
+ * Camera Utils
+ */
+export { getCameraCompassDirection, getCompassDirection, CompassDirections } from './camera/compass-directions'
 
 /**
  * UI
  */ 
-import { ImageSlicer, IImgSlice, IImgSliceProps, IImgSliceData } from './ui/classes/ImageSlicer'
-import { WrappedDynamicContainer, IWrappedDynamicContainerOptions, defaultWrappedDynamicContainerProps } from './ui/classes/WrappedDynamicContainer'
-import { WrappedDynamicImage, IWrappedDynamicImageOptions, defaultWrappedDynamicImageProps } from './ui/classes/WrappedDynamicImage'
-import { WrappedImage, IWrappedImageOptions, defaultWrappedImageProps } from './ui/classes/WrappedImage'
-import { noSign } from './ui/images/noSign'
+export { ImageSlicer, IImgSlice, IImgSliceProps, IImgSliceData } from './ui/classes/ImageSlicer'
+export { WrappedDynamicContainer, IWrappedDynamicContainerOptions, defaultWrappedDynamicContainerProps } from './ui/classes/WrappedDynamicContainer'
+export { WrappedDynamicImage, IWrappedDynamicImageOptions, defaultWrappedDynamicImageProps } from './ui/classes/WrappedDynamicImage'
+export { WrappedImage, IWrappedImageOptions, defaultWrappedImageProps } from './ui/classes/WrappedImage'
+export { noSign } from './ui/images/noSign'
 
 /**
  * Various Utils
  */
 
-import { 
+export { 
   //string
   removeLineBreaks,
   removeZeroWidthSpaces,
@@ -100,92 +105,10 @@ import {
 /**
  * Other stuff
  */
-import { userInfo, initUserInfo } from './utils/userInfo'
+export { userInfo, initUserInfo } from './utils/userInfo'
 
 /**
  * Future dash
  */
-import { Dash_PaginatedList } from './entities/Dash_Future/PaginatedList/PaginatedList'
+export { Dash_PaginatedList } from './entities/Dash_Future/PaginatedList/PaginatedList'
 
-
-
-export {
-
-  AlertSystem,
-  CallbackDebouncer,
-  OnMove,
-  SignedFetchAPI,
-  SoundPlayer,
-  TransformGatherer,
-  UserInfo,
-
-  AnimatedModel,
-  Booth, IBoothProps,
-  Elements,
-  Image,
-  Model, IModelProps,
-  ModelGroup, IModelGroupProps,
-  POAPBooth, IPOAPBoothProps,
-  Sound,
-  WearableBooth, IWearableBoothProps,
-  ZooTools_Metronome,
-  ZooTools_Metronome_ISubscription,
-  ZooTools_ControlBoard,
-  ZooTools_ControlBoardButton,
-  ZooTools_ControlBoardMarker,
-  ZooTools_ControlBoardOutput,
-
-  ISystemCallback,
-  Interval,
-  PersistUntil,
-  RotateSystem,
-  ZooTools_AutoDanceZone, ZooTools_AutoEmoter, AutoEmoteSystem,
-  ImageSlicer, IImgSlice, IImgSliceProps, IImgSliceData,
-  WrappedDynamicContainer, IWrappedDynamicContainerOptions, defaultWrappedDynamicContainerProps,
-  WrappedDynamicImage, IWrappedDynamicImageOptions, defaultWrappedDynamicImageProps,
-  WrappedImage, IWrappedImageOptions, defaultWrappedImageProps,
-  noSign,
-
-  removeLineBreaks,
-  removeZeroWidthSpaces,
-  sanitizeInputString,
-  b58,
-  makeid, IMakeIdOptions, defaultMakeIdOptions,
-  proper,
-  lc,
-  uc,
-  truncate,
-  rgbToHex,
-  b2a,
-  a2b,
-  parse,
-  repeat,
-  //number
-  randomInt,
-  round,
-  roundPad,
-  formatNumber,
-  formatCompactNumber,
-  //array
-  first,
-  last,
-  sample,
-  pluck,
-  groupBy,
-  createList,
-  range,
-  //object
-  objectAssign,
-  //date
-  isDateValid,
-  //dcl
-  urn,
-  limitations,
-  getSceneLimitations,
-  //utils grouped by type
-  zootils,
-
-  userInfo, initUserInfo,
-
-  Dash_PaginatedList,
-}
